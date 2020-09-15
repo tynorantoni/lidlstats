fa4142bc41059ac71bbfc64d0feb3ed5f57ce68e
Pawel Szymaszek <pawel.szymaszek@gmail.com>
2020-09-16 00:36:15 +0200

    ocr and json class

--- lidlstats/lidlstatsApp/receiptocr.py
+++ lidlstats/lidlstatsApp/receiptocr.py
@@ -1,62 +1,71 @@
-import cv2
 import pytesseract
 from PIL import Image
 import json
 import re
 
-pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
-img = Image.open('lidlstatsPics/11.jpg')
-img2 = cv2.imread('lidlstatsPics/11.jpg')
 
-text = pytesseract.image_to_string(img)
+class ReceiptOCR():
 
-data_rows = text.split('\n')
-data_row_dict = {}
-step = 0
-id_row = 0
-sale = ''
-# Jaja wol.wyb.M,L, XL 1 * 6,99 6,99 C
-for i in data_rows:
-    step += 1
-    if re.match('\d+-\d+-\d+', i):
-        for j in data_rows[step:]:
-            if re.match('(PTU)', j):
-                break
-            if len(j.split(' ')) <= 5:
 
-                try:
-                    sale = re.search('(......)$',j).group(1)
-                except AttributeError:
-                    sale=''
+    def get_text_from_receipt(self, receipt, img_dir):
 
-            else:
-                id_row += 1
-                temporary_data_row = j.split(' ')
-                name = temporary_data_row[:-5]
-                try:
-                    if re.match('\d+', temporary_data_row[-4]):
-                        amount = temporary_data_row[-4]
+        pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
+        img = Image.open(img_dir + receipt)
+        text = pytesseract.image_to_string(img)
+        data_rows = text.split('\n')
+
+        return data_rows
+
+    def to_json_from_list(self, list_from_receipt):
+
+        data_row_dict = {}
+        step = 0
+        id_row = 0
+        # sale = ''
+
+        for i in list_from_receipt:
+            step += 1
+            if re.match('\d+-\d+-\d+', i):
+                for j in list_from_receipt[step:]:
+                    if re.match('(PTU)', j):
+                        break
+                    if len(j.split(' ')) <= 5:
+
+                        try:
+                            sale = re.search('(......)$', j).group(1)
+                            data_row_dict[id_row] = {'name': name,
+                                                           'amount': amount,
+                                                           'price': price,
+                                                           'sale': sale,
+                                                           'VAT': vat
+                                                     }
+                            sale=''
+
+                        except AttributeError:
+                            sale = ''
+
                     else:
-                        amount = temporary_data_row[-5]
-                except:
-                    print('shit happened', id_row, temporary_data_row)
-                price = temporary_data_row[-3]
-                vat = temporary_data_row[-1]
+                        id_row += 1
+                        temporary_data_row = j.split(' ')
+                        name = temporary_data_row[:-5]
+                        try:
+                            if re.match('\d+', temporary_data_row[-4]):
+                                amount = temporary_data_row[-4]
+                            else:
+                                amount = temporary_data_row[-5]
+                        except:
+                            print('shit happened', id_row, temporary_data_row)
+                        price = temporary_data_row[-3]
+                        vat = temporary_data_row[-1]
 
-                data_row_dict.update({id_row: {'name': name,
-                                          'amount': amount,
-                                          'price': price,
-                                          'sale': sale,
-                                          'VAT': vat}
-                                 })
-                temporary_data_row.clear()
+                        data_row_dict.update({id_row: {'name': name,
+                                                       'amount': amount,
+                                                       'price': price,
+                                                       'sale': sale,
+                                                       'VAT': vat}
+                                              })
+                        temporary_data_row.clear()
 
-print(data_row_dict)
-#
-# dżejPl = json.dumps(finaldata)
-dżej = json.dumps(data_row_dict)
-#
-# print('Pl\n',dżejPl)
-print('\nEcce Dżejson\n',dżej)
-
-
+        receipt_json = json.dumps(data_row_dict)
+        print(receipt_json)
+        return receipt_json
