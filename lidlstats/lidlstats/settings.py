8b355fd5ee10e1512ed1ab907b1ceb979325e1fc
Pawel Szymaszek <pawel.szymaszek@gmail.com>
2020-09-17 22:56:23 +0200

    making a model

--- lidlstats/lidlstats/settings.py
+++ lidlstats/lidlstats/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'lidlstatsApp.apps.LidlstatsappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- lidlstats/lidlstatsApp/models.py
+++ lidlstats/lidlstatsApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+
+class BasicDataModel(models.Model):
+    date_of_shopping = models.DateField(default=datetime.date.today)
+    product_data = models.JSONField(default=dict)
+    user_mail = models.CharField(max_length=100, default="none@none.none")
+    total_cost = models.FloatField(default=0.00)
+    vat_a = models.FloatField(default=0.00)
+    vat_b = models.FloatField(default=0.00)
+    vat_c = models.FloatField(default=0.00)
+
+    def __str__(self):
+        return self
