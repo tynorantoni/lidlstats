ffa44d1ab39c9b19c03a727cfbbeb0561d236433
Pawel Szymaszek <pawel.szymaszek@gmail.com>
2020-10-19 02:08:22 +0200

    details - add graphs

--- lidlstats/lidlstatsApp/views.py
+++ lidlstats/lidlstatsApp/views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
-
 from .forms import RegisterForm, UploadedImageForm
 from .filehandler import FileHandler
 from .models import CalculatedDataModel, BasicDataModel
@@ -29,7 +28,7 @@ def index(request):
                'min_cost_date': min_cost[1],
                'max_cost': round(max_cost[0], 2),
                'max_cost_date': max_cost[1],
-               'list_of_all_shoppings':list_of_all_shoppings
+               'list_of_all_shoppings': list_of_all_shoppings
                }
 
     return render(request, 'lidlstatsApp/index.html', context)
@@ -43,37 +42,53 @@ def upload_file(request):
             form.save()
 
             img_obj = form.instance
-            msg='Paragon został dodany, wkrótce zostanie przetworzony'
+            msg = 'Paragon został dodany, wkrótce zostanie przetworzony'
             return render(request, 'lidlstatsApp/upload.html', {'form': form,
-                                                                'img_obj':img_obj,
-                                                                'msg':msg,
-                                                                'list_of_all_shoppings':list_of_all_shoppings
+                                                                'img_obj': img_obj,
+                                                                'msg': msg,
+                                                                'list_of_all_shoppings': list_of_all_shoppings
                                                                 })
     else:
         form = UploadedImageForm()
-    return render(request, 'lidlstatsApp/upload.html', {'form': form, 'list_of_all_shoppings':list_of_all_shoppings})
+    return render(request, 'lidlstatsApp/upload.html', {'form': form, 'list_of_all_shoppings': list_of_all_shoppings})
 
 
 def detail_of_shopping(request, id_of_shopping):
     list_of_all_shoppings = BasicDataModel.objects.all()
     shopping_db_record = list_of_all_shoppings.get(pk=id_of_shopping)
-     # = BasicDataModel.objects.get(pk=id_of_shopping)
+    shopping_data = CalculatedDataModel.objects.get(shoppig_id=id_of_shopping)
 
     table_df = StatisticDevil()
     column_names = {'name': 'Nazwa Produktu', 'amount': 'Ilość', 'price': 'Cena', 'sale': 'rabat', 'VAT': 'VAT'}
-    table_to_show = table_df.make_yourself_a_table(shopping_db_record.product_data).rename(columns=column_names).to_html(
+    table_to_show = table_df.make_yourself_a_table(shopping_db_record.product_data).rename(
+        columns=column_names).to_html(
         classes='table table-light table-bordered table-hover table-striped ',
         justify='left'
     )
+    no_of_bought_items = len(table_df.make_yourself_a_table(shopping_db_record.product_data))
+    total_vat = round((shopping_data.vat_a + shopping_data.vat_b + shopping_data.vat_c), 2)
+    net_value = round((shopping_data.total_cost - total_vat), 2)
+    tax_value = round(((total_vat / shopping_data.total_cost) * 100), 2)
+    vat_a_value = round(((shopping_data.vat_a / total_vat) * 100), 2)
+    vat_b_value = round(((shopping_data.vat_b / total_vat) * 100), 2)
+    vat_c_value = round(((shopping_data.vat_c / total_vat) * 100), 2)
+
+    values_of_shopping = {'total_vat': total_vat,
+                          'net_value': net_value,
+                          'tax_value': tax_value,
+                          'vat_a_value': vat_a_value,
+                          'vat_b_value': vat_b_value,
+                          'vat_c_value': vat_c_value,
+                          'no_of_bought_items': no_of_bought_items}
 
     context = {'table_to_show': table_to_show,
-               'shopping_bd_record':shopping_db_record,
-               'list_of_all_shoppings': list_of_all_shoppings
+               'shopping_bd_record': shopping_db_record,
+               'list_of_all_shoppings': list_of_all_shoppings,
+               'shopping_data': shopping_data,
+               'values_of_shopping': values_of_shopping
                }
 
     return render(request, 'lidlstatsApp/details.html', context)
-
-
 
 
 def user_settings(request):
@@ -93,4 +108,3 @@ def register(response):
         form = RegisterForm()
 
     return render(response, 'registration/register.html', {'form': form})
-
